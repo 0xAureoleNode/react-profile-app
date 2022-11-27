b10d4681da33b3f18f868619c4cbbf5eb9e93fb7
”0xAureoleNode“ <aureolenode@gmail.com>
2022-11-27 11:43:14 +0800

    added profileList css module

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import styles from './App.module.css';
 import ProfileList from './components/profile/ProfileList';
-import { AiFillGithub } from 'react-icons/ai';
 
 function App() {
   return (
     <div>
       <ProfileList />
-      <AiFillGithub />
     </div>
   );
 }
--- src/components/profile/ProfileList.js
+++ src/components/profile/ProfileList.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import styles from './ProfileList.module.css';
+import Profile from './Profile';
 
 const ProfileList = () => {
-  return <div>ProfileList</div>;
+  return (
+    <section className={styles['center']}>
+      <div>
+        <h1>Team Members</h1>
+        <div className={styles['profile-container']}>
+          <Profile />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ProfileList;
